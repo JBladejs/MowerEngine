29e0cf1dc96e707854bea38a8682838b4c39875e
Bladejs <JBladejs@gmail.com>
2021-09-02 22:03:42 +0200

    Minor change.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
 #include "Mower.h"
-#include "graphics/util/Color.h"
-#include "util/Bag.h"
-#include "logic/ecs/Entity.h"
-#include "logic/ecs/SystemManager.h"
 
 //TODO: try to convert as many pointers to values as possible
 
