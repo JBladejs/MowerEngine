ab012d7b2028f653b23a105904eba4d35376d4e7
Bladejs <JBladejs@gmail.com>
2021-09-12 23:25:58 +0200

    Started working on ScriptManager.

--- src/scripting/ScriptManager.cpp
+++ src/scripting/ScriptManager.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "ScriptManager.h"
+
+void ScriptManager::init() {
+    lua.open_libraries(sol::lib::base,
+                       sol::lib::package,
+                       sol::lib::math,
+                       sol::lib::table);
+    //TODO: bind classes here
+
+}
