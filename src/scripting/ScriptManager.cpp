b525c5b435bd8d30b758f220e440a61d3ef654a4
Bladejs <JBladejs@gmail.com>
2021-09-12 22:04:29 +0200

    Added ScriptManager class.

---
+++ src/scripting/ScriptManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by JJBla on 9/12/2021.
+//
+
+#include "ScriptManager.h"
---
+++ src/scripting/ScriptManager.h
@@ -0,0 +1,15 @@
+//
+// Created by JJBla on 9/12/2021.
+//
+
+#ifndef MOWERENGINE_SCRIPTMANAGER_H
+#define MOWERENGINE_SCRIPTMANAGER_H
+
+
+class ScriptManager {
+public:
+
+};
+
+
+#endif //MOWERENGINE_SCRIPTMANAGER_H
