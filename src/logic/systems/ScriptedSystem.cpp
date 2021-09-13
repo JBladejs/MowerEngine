ce40ae9502331d6a3cb088d664955e750a2a7741
Bladejs <JBladejs@gmail.com>
2021-09-14 01:26:44 +0200

    Added skeleton for ScriptedSystem

---
+++ src/logic/systems/ScriptedSystem.cpp
@@ -0,0 +1,17 @@
+//
+// Created by JJBla on 9/14/2021.
+//
+
+#include "ScriptedSystem.h"
+
+void ScriptedSystem::initialize() {
+
+}
+
+void ScriptedSystem::render() {
+
+}
+
+void ScriptedSystem::update() {
+
+}
