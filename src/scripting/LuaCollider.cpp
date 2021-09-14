925b56485f87edbff8badb20785e1622855280f5
Bladejs <JBladejs@gmail.com>
2021-09-14 02:20:50 +0200

    Implemented Collider bindings.

--- src/scripting/LuaCollider.cpp
+++ src/scripting/LuaCollider.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "LuaCollider.h"
+
+void LuaCollider::bind(sol::state& lua) {
+    lua.new_usertype<BoxCollider>("BoxCollider",
+                                   "x", &BoxCollider::x,
+                                   "y", &BoxCollider::y,
+                                   "w", &BoxCollider::w,
+                                   "h", &BoxCollider::h
+                                   );
+
+    lua.new_usertype<CircleCollider>("CircleCollider",
+                                   "x", &CircleCollider::x,
+                                   "y", &CircleCollider::y,
+                                   "r", &CircleCollider::r
+    );
+}
