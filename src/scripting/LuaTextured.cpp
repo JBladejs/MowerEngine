894fef95167c393aaba9c67c60e1e2c1bb02cf6f
Bladejs <JBladejs@gmail.com>
2021-09-14 02:36:46 +0200

    Implemented Lua Textured binding.

--- src/scripting/LuaTextured.cpp
+++ src/scripting/LuaTextured.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "LuaTextured.h"
+
+void LuaTextured::bind(sol::state &lua) {
+    lua.new_enum("ScalingType",
+                 "NONE", NONE,
+                 "FRACTIONAL", FRACTIONAL,
+                 "CONSTANT", CONSTANT);
+
+    lua.new_usertype<Textured>("Textured",
+                               "texture", &Textured::texture,
+                               "scalingType", &Textured::scalingType,
+                               "scalingA", &Textured::scalingA,
+                               "scalingB", &Textured::scalingB);
+}
