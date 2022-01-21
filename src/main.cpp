5fa6a57898f1983d0c123299a3f60c8c24bda34f
goon <JBladejs@gmail.com>
2022-01-21 16:43:34 +0100

    Added lua test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 
 #include "Engine.h"
+#include "sol/sol.hpp"
 
 int main(int argc, char *args[]) {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+    lua.script("print('testing lua')");
     auto engine = Engine();
     engine.start();
     return engine.getExitCode();
